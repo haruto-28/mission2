dd240dd8b294e42517e1e7c80c02b2c2388c5053
harutowatarai <harutowatarai@gmail.com>
2025-02-01 12:14:01 +0900

    URL_password

--- pages/2_AI.py
+++ pages/2_AI.py
@@ -1,2 +1,12 @@
 import streamlit as st
-st.write("https://dot-labo-st-assistant-demo.streamlit.app/")
+# パスワードを設定
+correct_password = "passnyan"
+
+# ユーザーからの入力を受け取る
+user_input = st.text_input("パスワードを入力してください:")
+
+# 入力した文字がパスワードと同じかチェック
+if user_input == correct_password:
+    st.write("https://dot-labo-st-assistant-demo.streamlit.app/")  # 一致したら「OK」と表示
+else:
+    st.write("違います")  # 一致しなかったら「違います」と表示
