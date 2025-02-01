4a39febf271476d3f0979d6957439ac58e99ac70
harutowatarai <harutowatarai@gmail.com>
2025-02-01 12:22:13 +0900

    Standby

--- pages/2_AI.py
+++ pages/2_AI.py
@@ -1,6 +1,7 @@
 import streamlit as st
 # パスワードを設定
 correct_password = "passnyan"
+Nothing = ""
 
 # ユーザーからの入力を受け取る
 user_input = st.text_input("パスワードを入力してください:")
@@ -8,5 +9,7 @@ user_input = st.text_input("パスワードを入力してください:")
 # 入力した文字がパスワードと同じかチェック
 if user_input == correct_password:
     st.write("https://dot-labo-st-assistant-demo.streamlit.app/")  # 一致したら「OK」と表示
+elif user_input == Nothing:
+    st.write("・・・")
 else:
     st.write("違います")  # 一致しなかったら「違います」と表示
